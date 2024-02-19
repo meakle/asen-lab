e671a4b289e3f4874aac98d624ed71e8ad893394
meakle <meakle@qq.com>
2024-02-19 14:52:14 +0800

    fix: ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,5 +14,6 @@ export default defineConfig({
             enabled: true
         }
     }),
+    output: 'hybrid',
     integrations: [mdx(), sitemap(), tailwind({ applyBaseStyles: false }), vue()]
 });
